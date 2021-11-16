1db344368c824c5702713265dc6545081924f808
kiadr9372 <kiadr9372@gmail.com>
2021-11-16 14:50:46 +0200

    add redis subscriber

---
+++ src/Utilities/Prices/callbackmodel.ts
@@ -0,0 +1,12 @@
+
+export interface CallbackModel {
+    connected: any;
+    disconnected: any;
+    logged: any;
+    subscribed: any;
+    ping: any;
+    price: any;
+    data: any;
+    error: any;
+    event: any;
+}
